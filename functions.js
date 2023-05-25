8c845c592f12e886c2c4e94843fc00fc94f7bba1
Brian Galeano <108309802+CobyGaleano@users.noreply.github.com>
2023-05-25 13:09:32 -0300

    Function Java and copyright footer

---
+++ functions.js
@@ -0,0 +1,19 @@
+function redirection(tipe){
+    switch(tipe){
+        case "GranCerdo":
+            window.location.href = "https://github.com/CobyGaleano/GranCerdo";
+            break;
+        
+        case "Portfolio":
+            window.location.href = "https://cobygaleano.github.io/MyFirstWeb/";
+            break;
+
+        case "GestionArticulos":
+            window.location.href = "https://github.com/CobyGaleano/Prog3-TP2";
+            break;
+
+        case "BomberMan":
+            window.location.href = "https://github.com/CobyGaleano/Okay-Listo-Boom";
+            break;
+    }
+}

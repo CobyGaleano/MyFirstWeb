041674fecea949c1910f1a3294458a0a41d8481c
Brian Galeano <108309802+CobyGaleano@users.noreply.github.com>
2024-03-03 01:38:25 -0300

    .

--- functions.js
+++ functions.js
@@ -1,4 +1,33 @@
-function redirection(tipe){
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>
+{
+    sections.forEach(sec =>
+    {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');  
+            
+        if(top >= offset && top < offset + height)
+        {
+            navLinks.forEach(links =>
+            {
+                links.classList.remove('active');
+                document.querySelector('header navBar a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    let header = document.querySelector('header');
+    
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+}
+
+function redirection(tipe)
+{
     switch(tipe){
         case "GranCerdo":
             window.location.href = "https://github.com/CobyGaleano/GranCerdo";

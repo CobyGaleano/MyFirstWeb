86a2721a7b3521a99ae361e15c737edcd5e6c5f2
Brian Galeano <108309802+CobyGaleano@users.noreply.github.com>
2024-03-03 23:53:15 -0300

    Redireccionamiento entre pagina
    Agregado de estilos y cambios generales

--- functions.js
+++ functions.js
@@ -1,19 +1,15 @@
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () =>
-{
-    sections.forEach(sec =>
-    {
+window.onscroll = () => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');  
             
-        if(top >= offset && top < offset + height)
-        {
-            navLinks.forEach(links =>
-            {
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header navBar a[href*=' + id + ']').classList.add('active');
             });
